bea2f12fe4f9cc0c775d9e2b4d89a5c89a73d267
Dilettante258 <113875344+Dilettante258@users.noreply.github.com>
2023-02-16 19:51:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -221,6 +221,7 @@ def main():
         tbs = get_tbs(i)
         favorites = get_favorite(i)
         for j in favorites:
+            print(j)
             time.sleep(random.randint(1,5))
             client_sign(i, tbs, j.get("id"), i.get("name"))
         logger.info("完成第" + str(n) + "个用户签到")
